71621115cca0746145acce3f329e984945861a3c
David Löfstrand <davidlofstrand@gmail.com>
2024-02-28 20:28:03 +0100

    Initial commit

---
+++ python/README.md
@@ -0,0 +1,18 @@
+## 1 create environment "grpc_hello" in i.e. conda
+conda env create -f environment.yml
+
+## 2 switch
+conda activate grpc_hello
+
+## 3 build service code for both server and client
+python -m grpc_tools.protoc --proto_path=..\contracts ..\contracts\hello.proto --python_out=. --grpc_python_out=.
+
+## 4 Start server in one terminal (with conda env activated)
+python server.py
+
+
+## 5 Test with client in another terminal (with conda env activated)
+python client.py
+
+
+Press Ctrl+C to kill the server when done.
---
+++ python/server.py
@@ -0,0 +1,36 @@
+import grpc
+from concurrent import futures
+import time
+import chat_pb2_grpc as pb2_grpc
+import chat_pb2 as pb2
+
+
+class UnaryService(pb2_grpc.UnaryServicer):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def GetServerResponse(self, request, context):
+
+        # get the string from the incoming request
+        message = request.message
+        print(f"Received a message: {message}")
+        result = f'Hello I am up and running received "{message}" message from you'
+        result = {'message': result, 'received': True}
+
+        return pb2.MessageResponse(**result)
+
+
+def serve(port, max_workers):
+    print(f"Starting chat server on port: {port}, with max workers: {max_workers}")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
+    pb2_grpc.add_UnaryServicer_to_server(UnaryService(), server)
+    server.add_insecure_port(f'[::]:{port}')
+    server.start()
+    print(f"Server started...")
+    print(f"Waiting for termination")
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve(port=50051, max_workers=10)
